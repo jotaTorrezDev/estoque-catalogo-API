4e84ceeccac11e290e6a86a2757a3266dff5641d
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-22 15:42:00 -0300

    Add throttling settings for API requests

--- rdf/settings.py
+++ rdf/settings.py
@@ -34,6 +34,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '60/minute',
+    },
 }
 
 SPECTACULAR_SETTINGS = {
