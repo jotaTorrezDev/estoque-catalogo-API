d0af5d4df9a0e25ddfc80160b1dc599d5cd4b8c1
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-22 01:30:30 -0300

    Update SECRET_KEY to use a placeholder
    Replaced the actual secret key with a placeholder for security.

--- rdf/settings.py
+++ rdf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ti*wlz+8ocp&!7*vx#q2ygir-$qiepvrm_k3^t)t0%rsd2-1!'
+SECRET_KEY = '***************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
