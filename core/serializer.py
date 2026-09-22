a1183524764a7b84fcb3048c3631f5d2ee0eb595
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-22 03:45:19 -0300

    Refactor serializers to update field names

--- core/serializer.py
+++ core/serializer.py
@@ -8,18 +8,17 @@ class CategoriaSerializer(serializers.ModelSerializer):
         fields = ['id', 'nome', 'descricao']
 
 
-
 class ProdutoSerializer(serializers.ModelSerializer):
-        Categoria = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all())
+    Categoria = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all())
 
-        class Meta:
-            model = Produto
-            fields = ['id', 'nome', 'preco', 'estoque', 'Categoria']
+    class Meta:
+        model = Produto
+        fields = ['id', 'nome', 'preco', 'estoque', 'Categoria']
 
 
 class PedidosSerializer(serializers.ModelSerializer):
-    produtos = serializers.PrimaryKeyRelatedField(queryset=Produto.objects.all(), many=True)
+    produto = serializers.PrimaryKeyRelatedField(queryset=Produto.objects.all())
 
     class Meta:
         model = Pedidos
-        fields = ['id', 'nome_cliente', 'email_cliente', 'data_pedido', 'produtos']
+        fields = ['id', 'nome_cliente', 'produto', 'quantidade', 'data_pedido']
