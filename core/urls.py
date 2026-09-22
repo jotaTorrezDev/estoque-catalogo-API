35ca7f29daf52998ff5c51a8c4a6da9585e1c9bf
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-22 03:19:14 -0300

    Add API schema and Swagger documentation routes
    Added API schema and documentation endpoints.

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'produtos', views.ProdutoViewSet)
@@ -13,5 +13,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', obtain_auth_token),
     path('pedidos/', views.PedidosViewSet.as_view({'get': 'list', 'post': 'create'}), name='pedidos-list'),
-    
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
